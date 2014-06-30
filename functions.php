dd39316746cbb2cca0e33397320bfb81a313345e
idleberg <jan@idleberg.com>
2014-06-30 22:07:58 +0200

    disable admin bar

--- functions.php
+++ functions.php
@@ -7,11 +7,10 @@
  * @since wp-ratchet 0.0.1
  */
 
-if ( function_exists( 'add_theme_support' ) ) { 
-    add_theme_support( 'post-thumbnails' );
-    set_post_thumbnail_size( 42, 42, true );
-}
+// We clearly need no admin bar on mobile
+add_filter('show_admin_bar', '__return_false');
 
+// Add class names to pagination links (http://css-tricks.com/snippets/wordpress/add-class-to-links-generated-by-next_posts_link-and-previous_posts_link/)
 add_filter('next_posts_link_attributes', 'next_posts_attributes');
 add_filter('previous_posts_link_attributes', 'previous_posts_attributes');
 
@@ -20,6 +19,12 @@ function next_posts_attributes() {
 }
 function previous_posts_attributes() {
     return 'class="btn btn-link btn-nav pull-left" data-transition="slide-out"';
+}
+
+// resize thumbnails to 42x42 (doesn't seem to work yet)
+if ( function_exists( 'add_theme_support' ) ) { 
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 42, 42, true );
 }
 
 ?>
